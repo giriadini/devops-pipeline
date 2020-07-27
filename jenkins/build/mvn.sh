7ecf00aceeff45e4d1e0ec7af7320ed4eeb552de
giriadini <giri62535@gmail.com>
2020-07-27 20:38:25 +1000

    modified paths

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "****************************"
 echo "**** Building jar ****"
 echo "****************************"
 
-WORKSPACE=/Users/giriadini/Documents/Docker-Images/jenkins/jenkins_home/workspace
+WORKSPACE=/Users/giriadini/Documents/Docker-Images/jenkins/jenkins_home/workspace/jenkins-pipeline
 
 docker run --rm  -v $WORKSPACE/gitClient:/app -v /Users/giriadini/dockermount_share/.m2:/root/.m2 -w /app maven:3-alpine "$@"
