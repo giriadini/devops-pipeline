46ad6de71e875db8c0f5d69ff8d9eca25e363f71
giriadini <giri62535@gmail.com>
2020-07-27 20:27:34 +1000

    Application integrated with jenkins

---
+++ jenkins/test/mvn.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "****************************"
+echo "**** testing the jar ****"
+echo "****************************"
+
+WORKSPACE=/Users/giriadini/Documents/Docker-Images/jenkins/jenkins_home/workspace
+
+docker run --rm  -v $WORKSPACE/gitClient:/app -v /Users/giriadini/dockermount_share/.m2:/root/.m2 -w /app maven:3-alpine "$@"
